5173533f5887dcf3242d6108ee15f07871241884
Launa Bodde <labodde@microsoft.com>
2020-08-28 15:30:22 -0700

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,44 @@
+import React, {useEffect, useState} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const callBackendAPI = async () => {
+  const response = await fetch('/api');
+  const body = await response.json();
+
+  if (response.status !== 200) {
+    throw Error(body.message) 
+  }
+  return body;
+};
+
+function App() {
+  const [apiTest, setAPITest] = useState('TEST');
+  useEffect(() => {
+    callBackendAPI()
+      .then(res => setAPITest({ data: res.express }))
+      .catch(err => console.log(err));
+  });
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <div><h1>{apiTest.data}</h1></div>
+    </div>
+  );
+}
+
+export default App;
