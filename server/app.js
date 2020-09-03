b96c96889efc46124e98987828203f02877e77c6
Launa Bodde <labodde@microsoft.com>
2020-09-02 18:20:40 -0700

    changing app.js

--- server/app.js
+++ server/app.js
@@ -30,9 +30,9 @@ app.get('/api/connected', (req, res) => {
 });
 
 // Serve the React application
-app.use(express.static(path.join(__dirname, 'static')));
+app.use(express.static('static'));
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join('index.html'));
 });
 
 const port = normalizePort(process.env.PORT || 5000);
