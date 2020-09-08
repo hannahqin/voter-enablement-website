a772db562aa3cb2a39251d9dce090b646e1db51e
Launa Bodde <labodde@microsoft.com>
2020-09-07 20:45:51 -0700

    commiting build

---
+++ client/build/precache-manifest.458257a30047ed72ef5df948bf7edbb6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67f7449d3c179e530f5a42678b57582e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f9b3f328f3dc9ca44da",
+    "url": "/static/css/main.bdca36d6.chunk.css"
+  },
+  {
+    "revision": "2976e19cbfbea57600c4",
+    "url": "/static/js/2.4089cd83.chunk.js"
+  },
+  {
+    "revision": "177dce6ef9045384a932287ff9d95cbd",
+    "url": "/static/js/2.4089cd83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f9b3f328f3dc9ca44da",
+    "url": "/static/js/main.1ce1c3a7.chunk.js"
+  },
+  {
+    "revision": "1507caca233df07c4fde",
+    "url": "/static/js/runtime-main.9e99343a.js"
+  },
+  {
+    "revision": "16e1fc01692d76f6f5e20c1904223dd3",
+    "url": "/static/media/FactoriaUltra.16e1fc01.otf"
+  }
+]);
