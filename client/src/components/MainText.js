382948cd31c692ff87edbf202238ab051abb1372
Hannah Qin <9788775+hannahqin@users.noreply.github.com>
2020-09-12 14:25:54 -0700

    adjusted textloop spacing to be more responsive and removed some code that's no longer relevant

--- client/src/components/MainText.js
+++ client/src/components/MainText.js
@@ -20,7 +20,7 @@ export function MainText() {
     <React.Fragment>
         <Grid item xs={12} md={12} lg={12}>
           <Typography variant="h6" style={{fontWeight:'400', width:'100%'}}>WE VOTE FOR</Typography>
-          <TextLoop interval='1500' noWrap={false} mask={true}>
+          <TextLoop interval='2000' noWrap={false} mask={true}>
             <Typography variant="h2" className={classes.root}><strong>CHANGE </strong> <small>vs. </small>STATUS QUO</Typography>
             <Typography variant="h2" className={classes.root}><strong>HOPE </strong> <small>vs. </small>FEAR</Typography>
             <Typography variant="h2" className={classes.root}><strong>ACTION </strong> <small>vs. </small>TALK</Typography>
@@ -33,26 +33,6 @@ export function MainText() {
             <Typography variant="h2" className={classes.root}><strong>GROWTH </strong> <small>vs. </small>STAGNATION</Typography>
             <Typography variant="h2" className={classes.root}><strong>JUSTICE </strong> <small>vs. </small>INJUSTICE</Typography>
           </TextLoop>
-        </Grid>
-    </React.Fragment>
-  );
-}
-
-export function SmallMainText() {
-    const classes=useStyles();
-
-  return (
-    <React.Fragment>
-        <Grid item xs={12} md={12} lg={12}>
-            <Typography variant="h4" className={classes.root}>WE VOTE FOR:</Typography>
-            <Typography variant="h4" className={classes.root}><strong>CHANGE </strong> <small>vs. </small>STATUS QUO /</Typography>
-            <Typography variant="h4" className={classes.root}><strong>HOPE </strong><small>vs.</small> FEAR / <strong>ACTION </strong> <small>vs. </small> TALK / </Typography>
-            <Typography variant="h4" className={classes.root}><strong>LOVE </strong> <small>vs. </small>HATE /</Typography>
-            <Typography variant="h4" className={classes.root}><strong>OPTIMISM </strong><small>vs. </small>PESSIMISM / </Typography>
-            <Typography variant="h4" className={classes.root}><strong>JUSTICE </strong><small>vs. </small>INJUSTICE /</Typography>
-            <Typography variant="h4" className={classes.root}><strong>UNITY</strong> <small>vs. </small> DIVISION /</Typography>
-            <Typography variant="h4" className={classes.root}><strong>COMPASSION </strong> <small>vs. </small>NARCISSISM / </Typography>
-            <Typography variant="h4" className={classes.root}><strong>OPPORTUNITY </strong><small>vs. </small> OPPRESSION</Typography>
         </Grid>
     </React.Fragment>
   );
